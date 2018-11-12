3354076e198ea57db629b0be1af66c41dd76b438
Wilson Lee <wilsonlee0714@gmail.com>
2018-11-12 12:03:09 +0800

    select

---
+++ menu_test01.php
@@ -0,0 +1,43 @@
+<?php
+require __DIR__.'/__connect_db.php';
+
+$sql = "SELECT * FROM `categories`";
+$stmt = $pdo->query($sql);
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$m_data = [];
+
+foreach($data as $v){
+    // 如果是第一層選單
+    if($v['parent_sid']==0){
+        $m_data[$v['sid']] = $v;
+    }
+}
+foreach($data as $v){
+    // 如果是第二層選單
+    // 如果這項資料的 parent_sid 是 m_data 的 key, 表示它是第二層
+    if(isset($m_data[$v['parent_sid']])){
+        $m_data[$v['parent_sid']]['children'][] = $v;
+    }
+}
+
+
+$result =[
+    'success'=>true,
+    'data'=>$m_data,
+];
+
+echo json_encode($result, JSON_UNESCAPED_UNICODE);
+
+
+
+
+
+
+
+
+
+
+
+
+
