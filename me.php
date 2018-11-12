5a3810ebd2cf05139b1e3d2ace8649ed0a24b050
Wilson Lee <wilsonlee0714@gmail.com>
2018-11-12 10:42:44 +0800

    修改個人資料

--- me.php
+++ me.php
@@ -7,6 +7,8 @@ $result = [
     'errorMsg' => '用戶沒有登入',
 ];
 
+$from_me = true; // 給被 included 的 php 檔使用
+
 if(! isset($_SESSION['user'])){
     echo json_encode($result, JSON_UNESCAPED_UNICODE);
     exit;
@@ -19,7 +21,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
         exit;
     case 'PUT':
         // 修改個人資料
-        //require __DIR__. '/me_edit.php';
+        require __DIR__. '/me_edit.php';
         exit;
     default:
         $result['resultCode'] = 401;
